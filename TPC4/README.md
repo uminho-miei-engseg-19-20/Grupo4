402c20deec753775aeaf490f0ab94e983fa86c30
Zayts3v <tiago-pinha@hotmail.com>
2020-03-23 17:55:30 +0000

    Feita a pergunta 2 e consequentemente o TPC4.

--- TPC4/README.md
+++ TPC4/README.md
@@ -43,4 +43,74 @@ koreCoin.addBlock(new Block (3, "23/03/2020", {amount: 70}));
 
 ### Pergunta 2.1
 
+Depois de alterar a variável da difculdade foi corrido o código para 4 difculdade diferentes.
+
+#### Dificuldade 2
+
+```
+Mining block 1...
+Block mined: 007abd29e589b6a35f6107095cbc8a3628b7a8ff9ac5f7a203f79b262fff077e
+Mining block 2...
+Block mined: 009d3e5e004957e04604a4032cc58a97f33adb6711f93bc7514de8fe4a531a37
+Mining block 3...
+Block mined: 004bb82a658a99fd8c9f0d40ec9e69d3a441dacb6383242659b1ff8f7b42195b
+
+real	0m0.172s
+user	0m0.168s
+sys	0m0.020s
+```
+
+#### Dificuldade 3
+
+```
+Mining block 1...
+Block mined: 0007c25169f5256ebb80fcbb423b582a8e699a759e41abb28906295973f9de4c
+Mining block 2...
+Block mined: 0004af57b70136e9f4b5309f92d518b3f297168d4cf47d4f9354808daadce457
+Mining block 3...
+Block mined: 00075796ab77bb5289bb9a952b91047482b3348d54d915ff5e5bce28256d01fe
+
+real	0m0.640s
+user	0m0.632s
+sys	0m0.044s
+```
+
+#### Dificuldade 4
+
+```
+Mining block 1...
+Block mined: 0000023168f87d968813b22c4dc92f60c127ff5084af8487d913d497ea7a7900
+Mining block 2...
+Block mined: 00008e0c291aaf728e015855328b14e651231cce209e6413503fd299e0df6c5e
+Mining block 3...
+Block mined: 0000fb4a126ef4c1c3c93bf2ed25e8db4c7da2ec89a46aad4f7bf092afd8b6b4
+
+real	0m3.130s
+user	0m3.028s
+sys	0m0.100s
+```
+
+#### Dificuldade 5
+
+```
+Mining block 1...
+Block mined: 0000023168f87d968813b22c4dc92f60c127ff5084af8487d913d497ea7a7900
+Mining block 2...
+Block mined: 000000b950a180294edddf4340a2d5834119a41bf89e4b2027f341f0fc02365e
+Mining block 3...
+Block mined: 0000088dc9c3115ee6ab7e95d2e8836f932d75d303ab451a825241acde589a58
+
+real	0m43.962s
+user	0m43.140s
+sys	0m0.744s
+```
+
 ### Pergunta 2.2
+
+#### 2.2.1
+
+O algoritmo ```proof of work```  consiste em incrementar (adicionar 1) ao valor do último número obtido no ```proof of work``` e continuar a incrementar o número até ser divisível por 9 e divisível pelo número anterior.
+
+#### 2.2.2
+
+O algoritmo não é o mais adequado. Primeiro, porque o cálculo da prova é realizado através da prova anterior, o que permite realizar provas antes do "tempo". Segundo, porque quando a *blockchain* atinge um tamanho considerável fica bastante complicado, e penoso computacional, encontrar um número divisível por 9 e pelo último **PoW**. Portanto, não achámos que este algoritmo seja o mais adequado.
--- TPC4/koreCoin/main.experiencia2.1.js
+++ TPC4/koreCoin/main.experiencia2.1.js
@@ -9,17 +9,17 @@ class Block{
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
-    
+
     calculateHash(){
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
     }
-    
+
     mineBlock(difficulty){
         while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
             this.nonce++;
             this.hash = this.calculateHash();
         }
-        
+
         console.log("Block mined: " + this.hash);
     }
 }
@@ -28,42 +28,42 @@ class Block{
 class Blockchain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 6;
+        this.difficulty = 5;
     }
-    
+
     createGenesisBlock(){
         return new Block(0, "02/01/2018", "Genesis Block", "0");
     }
-    
+
     getlatestBlock(){
         return this.chain[this.chain.length - 1];
     }
-    
+
     addBlock(newBlock){
         newBlock.previousHash = this.getlatestBlock().hash;
         newBlock.mineBlock(this.difficulty);
         //newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     } //In reality we cannot add a new block so easily. There are numerous checks in place like 'Proof of work', 'Proof of stake' etc.
-    
+
     isChainValid(){
         for(let i = 1; i < this.chain.length; i++){
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i-1];
-            
+
             if(currentBlock.hash !== currentBlock.calculateHash()){
                 return false;
             } //check for hash calculations
-            
+
             if(currentBlock.previousHash !== previousBlock.hash){
                 return false;
             } //check whether current block points to the correct previous block
-            
+
         }
-        
+
          return true;
     }
-    
+
 }
 
 
@@ -86,6 +86,6 @@ koreCoin.addBlock(new Block (3, "02/01/2018", {amount: 40}));
 
 //console.log(JSON.stringify(koreCoin, null, 4));
 
-//blockchain enables adding blocks to it. But never delete a block or make changes to an already created block. 
+//blockchain enables adding blocks to it. But never delete a block or make changes to an already created block.
 
 //What i've missed. Enabling rollback to a previous instance of the blockchain. Proof of work, a peer to peer network
