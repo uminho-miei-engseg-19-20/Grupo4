0bbe41267d2c95c2ac2e98ae0eb7ec1adc6c675c
Zayts3v <tiago-pinha@hotmail.com>
2020-07-05 22:22:43 +0100

    Nada.

--- projeto3/php/cmd_soap_msg.php
+++ projeto3/php/cmd_soap_msg.php
@@ -99,9 +99,9 @@ function ccmovelmultiplesign($client, $args){
 function validate_otp($client, $args){
 
     $request_data = [
-        "ApplicationId" => utf8_encode($args["applicationId"]),
-        "ProcessId" => $args["processId"],
-        "OTP" => $args["otp"],
+        "applicationId" => utf8_encode($args["applicationId"]),
+        "processId" => $args["processId"],
+        "code" => $args["otp"],
     ];
 
     return $client->__soapCall("ValidateOtp",array($request_data));
